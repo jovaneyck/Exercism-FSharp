4d2433d102eb15b1f5e79cbf27802e0a57f28744
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-05-18 16:44:19 +0200

    moved some stuff around to make adding future exercises easier. also did difference of squares exercise

---
+++ difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,15 @@
+﻿module DifferenceOfSquares
+
+let squared n = n * n
+
+let squareOfSums n = 
+    [1..n]
+    |> Seq.sum
+    |> squared
+
+let sumOfSquares n = 
+    [1..n]
+    |> Seq.map squared
+    |> Seq.sum
+
+let difference n = squareOfSums n - sumOfSquares n
---
+++ sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,9 @@
+﻿module SumOfMultiples
+
+let sumOfMultiples numbers max = 
+    [1..max-1]
+    |> Seq.filter 
+        (fun candidate -> 
+            numbers 
+            |> Seq.exists (fun nb -> candidate % nb = 0))
+    |> Seq.sum
