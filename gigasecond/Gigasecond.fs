96f06653b8e7acffb342809ec02a170d7ff944e4
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-05-20 10:47:22 +0200

    gigasecond: + operator instead of pipe-heavy approach. Less composable but more readable

---
+++ gigasecond/Gigasecond.fs
@@ -0,0 +1,11 @@
+﻿module Gigasecond
+
+open System
+
+let gigasecond birthdate = 
+    let aGigaSecond = 10.0 ** 9.0
+    let (+) (date : DateTime) seconds = date.AddSeconds(seconds)
+    let onlyDate (dateTime : DateTime) = dateTime.Date
+
+    birthdate + aGigaSecond
+    |> onlyDate
