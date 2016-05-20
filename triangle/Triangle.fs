296b039c02a4ba2237244738852209d87b8816f8
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-05-20 12:40:13 +0200

    triangle passes tests but is a mess.

---
+++ triangle/Triangle.fs
@@ -0,0 +1,27 @@
+﻿module Triangle
+    type TriangleKind = 
+        | Equilateral
+        | Isosceles
+        | Scalene
+
+    let kind x y z = 
+        let sides = x :: y :: z :: [] 
+
+        let isNotATriangle =
+                x + y <= z || y + z <= x || x + z <= y
+
+        if sides |> Seq.exists (fun s -> s <= 0m) 
+        then 
+            raise <| new System.InvalidOperationException("All sides must be positive")
+        elif isNotATriangle
+        then
+            raise <| new System.InvalidOperationException("Not a valid triangle.")
+        else
+            let nbDifferentLengths = 
+                sides
+                |> Seq.groupBy id 
+                |> Seq.length
+            match nbDifferentLengths with
+            | 1 -> Equilateral
+            | 2 -> Isosceles
+            | _ -> Scalene
