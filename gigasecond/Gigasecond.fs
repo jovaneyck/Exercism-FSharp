56f0a92898301abe51cd9d99d773708aba75c31a
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-05-22 13:52:10 +0200

    renamed function to better reveal its intent

--- gigasecond/Gigasecond.fs
+++ gigasecond/Gigasecond.fs
@@ -5,7 +5,7 @@ open System
 let gigasecond birthdate = 
     let aGigaSecond = 10.0 ** 9.0
     let (+) (date : DateTime) seconds = date.AddSeconds(seconds)
-    let onlyDate (dateTime : DateTime) = dateTime.Date
+    let datePart (dateTime : DateTime) = dateTime.Date
 
     birthdate + aGigaSecond
-    |> onlyDate
+    |> datePart
