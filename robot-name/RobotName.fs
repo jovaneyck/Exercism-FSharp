4c5fb83020f9f953a8a41f2aee680abcb937e10d
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-05-22 15:13:14 +0200

    robot name

---
+++ robot-name/RobotName.fs
@@ -0,0 +1,28 @@
+﻿module RobotName
+
+open System
+
+let rng = new System.Random()
+
+let randomChar() = 
+    'A'
+    |> int
+    |> (fun a -> a + rng.Next(0,25))
+    |> char
+
+let randomNb() = rng.Next(0,10)
+
+let randomName() =
+    sprintf
+        "%c%c%d%d%d"
+        (randomChar ())
+        (randomChar ())
+        (randomNb ())
+        (randomNb ())
+        (randomNb ())
+
+type Robot() = 
+    let mutable name = randomName()
+    member this.Name = name
+    member this.Reset() = 
+        name <- randomName()
