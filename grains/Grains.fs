a6b2751c516cbfa5cd944d001266a27ff343f9a1
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-05-23 14:46:14 +0200

    grains

---
+++ grains/Grains.fs
@@ -0,0 +1,12 @@
+﻿module Grains
+
+let square n =
+    let rec sq acc n =
+        if n = 1 then acc
+        else sq (2I * acc) (n - 1)
+    sq 1I n
+
+let total = 
+    [1..64]
+    |> Seq.map square
+    |> Seq.sum
