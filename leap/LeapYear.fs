b9181f14af4642ce6945a6dfaa51a5044f255cae
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-05-23 16:39:50 +0200

    leap year. with partial active pattern matching instead of explicit encoding of the business rules

---
+++ leap/LeapYear.fs
@@ -0,0 +1,15 @@
+﻿module LeapYear
+
+let private divisibleBy divisor nb = 
+    nb % divisor = 0
+let private (|DivisibleBy|_|) d nb = 
+    if nb |> divisibleBy d
+    then Some DivisibleBy 
+    else None
+
+let isLeapYear year = 
+    match year with
+    | DivisibleBy 400 -> true
+    | DivisibleBy 100 -> false
+    | DivisibleBy 4   -> true
+    | _               -> false
---
+++ leap/LeapYearTests.fs
@@ -0,0 +1,26 @@
+﻿module LeapYearTests
+
+open NUnit.Framework
+open LeapYear
+    
+(*
+    on every year that is evenly divisible by 4
+    except every year that is evenly divisible by 100
+    unless the year is also evenly divisible by 400
+*)
+
+[<Test>]
+let ``Is 1996 a valid leap year`` () = 
+    Assert.That(isLeapYear 1996, Is.True)
+    
+[<Test>]
+let ``Is 1997 an invalid leap year`` () = 
+    Assert.That(isLeapYear 1997, Is.False)
+    
+[<Test>]
+let ``Is the turn of the 20th century an invalid leap year`` () = 
+    Assert.That(isLeapYear 1900, Is.False)
+    
+[<Test>]
+let ``Is the turn of the 25th century a valid leap year`` () = 
+    Assert.That(isLeapYear 2400, Is.True)
