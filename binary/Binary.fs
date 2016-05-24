a5ce100dd77db157e24dbb3fd20abdebb1d9522d
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-05-24 08:34:53 +0200

    binary

---
+++ binary/Binary.fs
@@ -0,0 +1,28 @@
+﻿module Binary
+
+let private without substr (str : string) =
+    str.Replace(substr, "")
+
+let private isValidBinary (input : string) =
+    let invalidChars = 
+        input 
+        |> without "1" 
+        |> without "0"
+    invalidChars 
+    |> Seq.isEmpty
+
+let private convert (input : string) =
+    input
+    |> Seq.map (string >> float)
+    |> Seq.rev
+    |> Seq.indexed
+    |> Seq.map 
+        (fun (idx, digit) -> 
+            let n = idx |> float
+            digit * 2.0 ** n)
+    |> Seq.sum
+    |> int
+
+let toDecimal input  = 
+    if not (isValidBinary input) then 0
+    else convert input
