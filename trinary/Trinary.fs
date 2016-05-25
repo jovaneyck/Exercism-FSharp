936cdae2f5ca3b4755d2c902066b0d171456f56a
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-05-25 11:43:27 +0200

    trinary

---
+++ trinary/Trinary.fs
@@ -0,0 +1,27 @@
+﻿module Trinary
+
+let private add first second = 
+    match first, second with
+    | Some f, Some s -> Some (f + s)
+    | _ -> None
+
+let private convert (input : string) =
+    input
+    |> Seq.rev
+    |> Seq.mapi
+        (fun index digit ->
+            let multiplier = pown 3 index
+            match digit with
+            | '0'           -> Some <| 0 * multiplier
+            | '1'           -> Some <| 1 * multiplier
+            | '2'           -> Some <| 2 * multiplier
+            | invalid       -> None)
+    |> Seq.reduce add
+
+let orDefault theDefault value = 
+    value 
+    |> defaultArg <| theDefault
+
+let toDecimal input  = 
+    convert input
+    |> orDefault 0
