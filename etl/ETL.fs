c130d9c68ad63455eb749d5251287dad3a0bed93
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-05-25 12:01:55 +0200

    ETL on maps

---
+++ etl/ETL.fs
@@ -0,0 +1,14 @@
+﻿module ETL
+
+open System
+
+let private toLower (s : String) = s.ToLower()
+
+let transform input = 
+    input
+    |> Map.toSeq
+    |> Seq.collect 
+        (fun (score, letters) -> 
+            letters 
+            |> Seq.map (fun letter -> (letter |> toLower, score)))
+    |> Map.ofSeq
