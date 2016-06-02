3457feb1ecf81b1cdc72e76a8b50754ec4b7459a
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-06-02 08:54:11 +0200

    word count. still have to work a bit on my regex-fu apparently

---
+++ word-count/WordCount.fs
@@ -0,0 +1,35 @@
+﻿module Phrase
+
+open System.Text.RegularExpressions
+
+let chunk phrase = Regex.Replace(phrase, "[^(\w|\')]", " ")
+
+let split (s : string) = 
+    s.Split([|" "|], System.StringSplitOptions.RemoveEmptyEntries)
+
+let erase regex phrase = 
+    Regex.Replace(phrase, regex, "")
+
+let toLower (s : string) = s.ToLower()
+
+let sanitize word = 
+    let charactersNotInAlphabet = "[^(\w|\')]"
+    let nonLettersAtStart = "^[^(\w)]*"
+    let nonLettersAtEnd = "[^(\w)]*$"
+
+    word
+    |> erase charactersNotInAlphabet
+    |> erase nonLettersAtStart
+    |> erase nonLettersAtEnd
+    |> toLower
+
+let containsALetter w = Regex.Match(w, "\w+").Success
+
+let wordCount phrase =
+    phrase
+    |> chunk
+    |> split
+    |> Seq.map (sanitize)
+    |> Seq.filter containsALetter
+    |> Seq.countBy id
+    |> Map.ofSeq
