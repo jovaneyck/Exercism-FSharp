7aa5eb70a1c7085e20759201095e8a9810b3ef14
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-06-03 11:46:08 +0200

    roman numerals. still not happy about the simplify step, lots of duplication

---
+++ roman-numerals/RomanNumeral.fs
@@ -0,0 +1,53 @@
+﻿module RomanNumeral
+
+open System
+
+let toString = List.toArray >> String
+
+//Let's do a two-phase algorithm as Sandi Metz shared in her latest newsletter!
+//http://us3.campaign-archive2.com/?u=1090565ccff48ac602d0a84b4&id=24e2bac263&e=445a4e222b
+
+let arabicToRoman = 
+    [
+        (1000, 'M')
+        (500, 'D')
+        (100, 'C')
+        (50, 'L')
+        (10, 'X')
+        (5, 'V')
+        (1, 'I')
+    ]
+    |> Map.ofSeq
+
+let keys =
+    arabicToRoman
+    |> Map.toList
+    |> List.sortByDescending (fun (key, _) -> key)
+    |> List.map (fun (key,_) -> key)
+
+let rec private expandedRoman acc arabic = 
+    if arabic = 0 then acc
+    else 
+        let biggestArabicThatFits = keys |> List.find (fun k -> arabic >= k)
+        let r = arabicToRoman |> Map.find biggestArabicThatFits
+        expandedRoman (acc @ [r]) (arabic - biggestArabicThatFits)
+
+let private simplify expanded = 
+    let rec simple acc expanded = 
+        match expanded with
+        | [] -> acc
+        | 'D'::'C'::'C'::'C'::'C'::rest -> simple (acc @ ['C';'M']) rest
+        | 'C'::'C'::'C'::'C'::rest -> simple (acc @ ['C';'D']) rest
+        | 'L'::'X'::'X'::'X'::'X'::rest -> simple (acc @ ['X';'C']) rest
+        | 'X'::'X'::'X'::'X'::rest -> simple (acc @ ['X';'L']) rest
+        | 'V'::'I'::'I'::'I'::'I'::rest -> simple (acc @ ['I';'X']) rest
+        | 'I'::'I'::'I'::'I'::rest -> simple (acc @ ['I';'V']) rest
+        | h::t -> simple (acc @ [h]) t
+
+    simple [] expanded
+
+let toRoman arabic =    
+    arabic
+    |> expandedRoman []
+    |> simplify
+    |> toString
---
+++ roman-numerals/RomanNumeralTests.fs
@@ -0,0 +1,35 @@
+﻿module RomanNumeralTests
+
+open NUnit.Framework
+open RomanNumeral
+    
+[<TestCase(0, ExpectedResult = "")>]
+[<TestCase(1, ExpectedResult = "I")>]
+[<TestCase(2, ExpectedResult = "II")>]
+[<TestCase(3, ExpectedResult = "III")>]
+[<TestCase(4, ExpectedResult = "IV")>]
+[<TestCase(5, ExpectedResult = "V")>]
+[<TestCase(6, ExpectedResult = "VI")>]
+[<TestCase(10, ExpectedResult = "X")>]
+[<TestCase(9, ExpectedResult = "IX")>]
+[<TestCase(27, ExpectedResult = "XXVII")>]
+[<TestCase(50, ExpectedResult = "L")>]
+[<TestCase(40, ExpectedResult = "XL")>]
+[<TestCase(48, ExpectedResult = "XLVIII")>]
+[<TestCase(59, ExpectedResult = "LIX")>]
+[<TestCase(100, ExpectedResult = "C")>]
+[<TestCase(90, ExpectedResult = "XC")>]
+[<TestCase(93, ExpectedResult = "XCIII")>]
+[<TestCase(141, ExpectedResult = "CXLI")>]
+[<TestCase(163, ExpectedResult = "CLXIII")>]
+[<TestCase(500, ExpectedResult = "D")>]
+[<TestCase(400, ExpectedResult = "CD")>]
+[<TestCase(402, ExpectedResult = "CDII")>]
+[<TestCase(575, ExpectedResult = "DLXXV")>]
+[<TestCase(1000, ExpectedResult = "M")>]
+[<TestCase(900, ExpectedResult = "CM")>]
+[<TestCase(911, ExpectedResult = "CMXI")>]
+[<TestCase(1024, ExpectedResult = "MXXIV")>]
+[<TestCase(3000, ExpectedResult = "MMM")>]
+let ``Convert roman to arabic numerals`` (arabicNumeral) =
+    toRoman arabicNumeral
