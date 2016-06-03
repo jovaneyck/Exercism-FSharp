0604fcc5c35b651e43d9185967cf1ed8b7ecc495
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-06-03 13:32:16 +0200

    accumulate: wrote my own map

---
+++ accumulate/Accumulate.fs
@@ -0,0 +1,8 @@
+﻿module Accumulate
+
+let accumulate f l = 
+    let rec accRecursive acc l =
+         match l with
+         | [] -> acc
+         | h :: t -> accRecursive (acc @ [f h]) t
+    accRecursive [] l
