7d53425604b99491f672456bf032ea05ae05df4f
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-06-03 13:19:51 +0200

    rna transcription

---
+++ rna-transcription/RNATranscription.fs
@@ -0,0 +1,14 @@
+﻿module RNATranscription
+
+let complements = 
+        [('G', 'C');('C', 'G');('T', 'A');('A', 'U')]
+        |> Map.ofSeq
+
+let complement nucleotide = complements |> Map.find nucleotide
+
+let toString = Seq.toArray >> System.String
+
+let toRna strand = 
+    strand 
+    |> Seq.map complement
+    |> toString
--- roman-numerals/RomanNumeral.fs
+++ roman-numerals/RomanNumeral.fs
@@ -2,49 +2,55 @@
 
 open System
 
-let toString = List.toArray >> String
+let private toString = List.toArray >> String
 
-//Let's do a two-phase algorithm as Sandi Metz shared in her latest newsletter!
+//Let's do a two-pass algorithm like the one Sandi Metz shared in her latest newsletter!
 //http://us3.campaign-archive2.com/?u=1090565ccff48ac602d0a84b4&id=24e2bac263&e=445a4e222b
 
-let arabicToRoman = 
+let private arabicToRoman = 
     [
-        (1000, 'M')
-        (500, 'D')
-        (100, 'C')
-        (50, 'L')
-        (10, 'X')
-        (5, 'V')
-        (1, 'I')
+        (1,     'I')
+        (5,     'V')
+        (10,    'X')
+        (50,    'L')
+        (100,   'C')
+        (500,   'D')
+        (1000,  'M')
     ]
     |> Map.ofSeq
 
-let keys =
-    arabicToRoman
+let private keys map =
+    map
     |> Map.toList
-    |> List.sortByDescending (fun (key, _) -> key)
     |> List.map (fun (key,_) -> key)
 
 let rec private expandedRoman acc arabic = 
     if arabic = 0 then acc
     else 
-        let biggestArabicThatFits = keys |> List.find (fun k -> arabic >= k)
-        let r = arabicToRoman |> Map.find biggestArabicThatFits
+        let biggestArabicThatFits = 
+                arabicToRoman 
+                |> keys 
+                |> List.sortDescending 
+                |> List.find (fun k -> arabic >= k)
+        let r = 
+                arabicToRoman 
+                |> Map.find biggestArabicThatFits
         expandedRoman (acc @ [r]) (arabic - biggestArabicThatFits)
 
 let private simplify expanded = 
-    let rec simple acc expanded = 
+    let rec simpler acc expanded = 
         match expanded with
         | [] -> acc
-        | 'D'::'C'::'C'::'C'::'C'::rest -> simple (acc @ ['C';'M']) rest
-        | 'C'::'C'::'C'::'C'::rest -> simple (acc @ ['C';'D']) rest
-        | 'L'::'X'::'X'::'X'::'X'::rest -> simple (acc @ ['X';'C']) rest
-        | 'X'::'X'::'X'::'X'::rest -> simple (acc @ ['X';'L']) rest
-        | 'V'::'I'::'I'::'I'::'I'::rest -> simple (acc @ ['I';'X']) rest
-        | 'I'::'I'::'I'::'I'::rest -> simple (acc @ ['I';'V']) rest
-        | h::t -> simple (acc @ [h]) t
+        //Not really happy with the noise here
+        | 'D'::'C'::'C'::'C'::'C'::rest -> simpler (acc @ ['C';'M']) rest
+        | 'C'::'C'::'C'::'C'::rest -> simpler (acc @ ['C';'D']) rest
+        | 'L'::'X'::'X'::'X'::'X'::rest -> simpler (acc @ ['X';'C']) rest
+        | 'X'::'X'::'X'::'X'::rest -> simpler (acc @ ['X';'L']) rest
+        | 'V'::'I'::'I'::'I'::'I'::rest -> simpler (acc @ ['I';'X']) rest
+        | 'I'::'I'::'I'::'I'::rest -> simpler (acc @ ['I';'V']) rest
+        | h::t -> simpler (acc @ [h]) t
 
-    simple [] expanded
+    simpler [] expanded
 
 let toRoman arabic =    
     arabic
