b52159f0798475d46546bd43d407e2e1584ad881
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-06-08 16:00:40 +0200

    strain with seqs instead of falling back to lists

--- strain/Strain.fs
+++ strain/Strain.fs
@@ -1,7 +1,9 @@
 module Seq
 
 let rec keep f xs = 
-    [for x in xs do 
-        if f x then yield x]
+    seq {
+        for x in xs do 
+            if f x then yield x
+    }
 
 let discard f = keep (f >> not)
