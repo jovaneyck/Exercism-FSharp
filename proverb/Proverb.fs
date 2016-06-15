5366105f9a3b8ae56d7ae1c3f82456e34ca3514f
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-06-15 08:50:02 +0200

    proverb. outofbound prone though

---
+++ proverb/Proverb.fs
@@ -0,0 +1,19 @@
+﻿module Proverb
+
+let words = 
+    ["nail"; "shoe"; "horse"; "rider"; "message"; "battle"; "kingdom"]
+
+let line number =
+    if number = 7 then
+        "And all for the want of a horseshoe nail."
+    else
+        match words.[number - 1 .. number] with
+        | [f; s] ->
+            sprintf "For want of a %s the %s was lost." f s
+        | _ -> 
+            failwith <| sprintf "don't know line %d" number
+
+let proverb = 
+    [1..7]
+    |> List.map line
+    |> String.concat "\n"
