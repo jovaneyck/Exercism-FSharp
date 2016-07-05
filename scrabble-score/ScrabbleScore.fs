be37922dd812b16782d91b812a5706a322cc2a4d
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-07-05 17:09:01 +0200

    scrabble score. one of the easy ones

---
+++ scrabble-score/ScrabbleScore.fs
@@ -0,0 +1,38 @@
+module Scrabble
+
+let letterScores = 
+ [
+    ('A', 1)
+    ('E', 1)
+    ('I', 1)
+    ('O', 1)
+    ('U', 1)
+    ('L', 1)
+    ('N', 1)
+    ('R', 1)
+    ('S', 1)
+    ('T', 1)
+    ('D', 2)
+    ('G', 2)
+    ('B', 3)
+    ('C', 3)
+    ('M', 3)
+    ('P', 3)
+    ('F', 4)
+    ('H', 4)
+    ('V', 4)
+    ('W', 4)
+    ('Y', 4)
+    ('K', 5)
+    ('J', 8)
+    ('X', 8)
+    ('Q', 10)
+    ('Z', 10)
+ ]
+ |> Map.ofSeq
+
+let score (word : string) = 
+    word.ToUpper()
+    |> Seq.map (fun letter -> letterScores |> Map.tryFind letter)
+    |> Seq.choose id
+    |> Seq.sum
