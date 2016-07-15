0af8d5355392d7c07f19c91d181b84b4e28a835c
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-07-15 10:18:05 +0200

    matrix

---
+++ matrix/Matrix.fs
@@ -0,0 +1,23 @@
+﻿module Matrix
+
+let parse = string >> System.Int32.Parse
+
+let fromString (input : string) = 
+    input.Split([|'\n'|])
+    |> Array.mapi (
+        fun rowNb line -> 
+            line.Split([|' '|])
+            |> Array.mapi (
+                fun colNb rawNb -> 
+                    ((rowNb, colNb), parse rawNb)))
+    |> Array.collect id
+
+let private getBy grouping matrix =
+    matrix
+    |> Array.groupBy grouping
+    |> Array.map (fun (_,group) -> 
+        group
+        |> Seq.map (fun (_, cell) -> cell))
+
+let rows matrix = getBy (fun ((r,_),_) -> r) matrix
+let cols matrix = getBy (fun ((_,c),_) -> c) matrix
