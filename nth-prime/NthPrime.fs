027058a4ac83115fd97a2788627e4c7ac3150a5b
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-08-05 09:45:55 +0200

    Nth prime using sieve of Eratosthenes. Sloooooow for large primes

---
+++ nth-prime/NthPrime.fs
@@ -0,0 +1,21 @@
+﻿module NthPrime
+
+//Literal sieve implementation using an infinite stream
+// not the fastest stuff out there.
+//8sec for 1.000th, ~infinity for 10.000th
+let primes = 
+    let rec primesRec candidates =
+        seq {
+            let nextPrime = candidates |> Seq.head
+            yield nextPrime
+
+            let remainingCandidates = 
+                candidates 
+                |> Seq.tail
+                |> Seq.filter (fun el -> el % nextPrime <> 0)   
+            yield! primesRec remainingCandidates
+        }
+    let initialCandidates = Seq.initInfinite <| (+) 2
+    primesRec initialCandidates
+
+let nthPrime n = primes |> Seq.item (n - 1)
