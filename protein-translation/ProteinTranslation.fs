d6b04fd4baa23c65b1e3fbc48a76b8b8df7483d6
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-08-30 16:57:22 +0200

    removed types as they were adding accidental complexity

--- protein-translation/ProteinTranslation.fs
+++ protein-translation/ProteinTranslation.fs
@@ -2,56 +2,37 @@
 
 open System
 
-type Protein =
-    | Methionine
-    | Phenylalanine
-    | Leucine
-    | Serine
-    | Tyrosine
-    | Cysteine
-    | Tryptophan
-    | STOP //Not the cleanest bit of domain modeling here.
-
-let translations = 
- [
-    ("AUG", Methionine)
-    ("UUU", Phenylalanine)
-    ("UUC", Phenylalanine)
-    ("UUA", Leucine)
-    ("UUG", Leucine)
-    ("UCU", Serine)
-    ("UCC", Serine)
-    ("UCA", Serine)
-    ("UCG", Serine)
-    ("UAU", Tyrosine)
-    ("UAC", Tyrosine)
-    ("UGU", Cysteine)
-    ("UGC", Cysteine)
-    ("UGG", Tryptophan)
-    ("UAA", STOP)
-    ("UAG", STOP)
-    ("UGA", STOP)
- ]
- |> Map.ofList
-
 let splitCodons (strand : string) =
     strand
     |> Seq.chunkBySize 3
     |> List.ofSeq
     |> List.map String
 
-let translateCodon c =
-    match translations |> Map.tryFind c with
-    | None -> failwithf "Unknown codon: %s" c
-    | Some codon -> codon
+let translateCodon = 
+    function
+    | "AUG" ->  "Methionine"
+    | "UUU"
+    | "UUC" -> "Phenylalanine"
+    | "UUA" 
+    | "UUG" ->  "Leucine"
+    | "UCU"
+    | "UCC"
+    | "UCA"
+    | "UCG" -> "Serine"
+    | "UAU"
+    | "UAC" -> "Tyrosine"
+    | "UGU"
+    | "UGC" -> "Cysteine"
+    | "UGG" -> "Tryptophan"
+    | "UAA"
+    | "UAG"
+    | "UGA" -> "STOP"
+    | c     -> failwithf "Unknown codon: %s" c
 
-let isStop c = c = STOP
-
-let toString = sprintf "%A"
+let isStop c = c = "STOP"
 
 let translate strand =
     strand 
     |> splitCodons
     |> List.map translateCodon
     |> List.takeWhile (not << isStop)
-    |> List.map toString
