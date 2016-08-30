a422d5e82943191fc5907c993f29e815c062b1c3
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-08-30 16:37:49 +0200

    got a working protein translation but not SRP

---
+++ protein-translation/ProteinTranslation.fs
@@ -0,0 +1,59 @@
+﻿module ProteinTranslation
+
+type Protein =
+    | Methionine
+    | Phenylalanine
+    | Leucine
+    | Serine
+    | Tyrosine
+    | Cysteine
+    | Tryptophan
+    | STOP //Not the cleanest bit of modeling here.
+
+let translations = 
+ [
+    ("AUG", Methionine)
+    ("UUU", Phenylalanine)
+    ("UUC", Phenylalanine)
+    ("UUA", Leucine)
+    ("UUG", Leucine)
+    ("UCU", Serine)
+    ("UCC", Serine)
+    ("UCA", Serine)
+    ("UCG", Serine)
+    ("UAU", Tyrosine)
+    ("UAC", Tyrosine)
+    ("UGU", Cysteine)
+    ("UGC", Cysteine)
+    ("UGG", Tryptophan)
+    ("UAA", STOP)
+    ("UAG", STOP)
+    ("UGA", STOP)
+ ]
+ |> Map.ofList
+
+let toString = sprintf "%A"
+
+let rec translateCodons acc codons =
+    match codons with
+    | [] -> acc
+    | c :: cs -> 
+        match translations |> Map.tryFind c with
+        | None -> failwithf "Unknown codon: %s" c
+        | Some c ->
+            match c with
+            | STOP -> acc
+            | translation -> translateCodons (translation :: acc) cs
+
+let splitCodons (strand : string) =
+    strand
+    |> Seq.chunkBySize 3
+    |> List.ofSeq
+    |> (List.map (System.String.Concat))
+
+let translate (strand : string) =
+    strand 
+    |> splitCodons
+    |> translateCodons []
+    |> List.rev
+    |> List.map toString
