68a92eaabad6bbec7711937e9918ab3c726205ab
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-09-01 16:04:10 +0200

    isogram

---
+++ isogram/Isogram.fs
@@ -0,0 +1,28 @@
+﻿module Isogram
+
+open System.Text.RegularExpressions
+
+let relevantCharacters = 
+    ['a'..'z'] @ ['A'..'Z'] @ ['à'..'Ȁ']
+let isRelevantCharacter ch =
+    relevantCharacters 
+    |> List.contains ch
+let onlyRelevantCharacters = 
+    Seq.filter isRelevantCharacter
+
+let toLower (str : string) = str.ToLower()
+
+let isogram (sentence : string) = 
+    let relevantCharacters =
+        sentence
+        |> toLower
+        |> onlyRelevantCharacters
+        |> List.ofSeq
+    let nbTotalLetters = 
+        relevantCharacters
+        |> List.length
+    let nbDifferentLetters =
+        relevantCharacters
+        |> List.groupBy id
+        |> List.length
+    nbTotalLetters = nbDifferentLetters
--- isogram/IsogramTest.fs
+++ isogram/IsogramTest.fs
@@ -5,14 +5,14 @@ open NUnit.Framework
 open Isogram
 
 [<TestCase("duplicates", ExpectedResult = true)>]
-[<TestCase("eleven", ExpectedResult = false, Ignore = "Remove to run test case")>]
-[<TestCase("subdermatoglyphic", ExpectedResult = true, Ignore = "Remove to run test case")>]
-[<TestCase("Alphabet", ExpectedResult = false, Ignore = "Remove to run test case")>]
-[<TestCase("thumbscrew-japingly", ExpectedResult = true, Ignore = "Remove to run test case")>]
-[<TestCase("Hjelmqvist-Gryb-Zock-Pfund-Wax", ExpectedResult = true, Ignore = "Remove to run test case")>]
-[<TestCase("Heizölrückstoßabdämpfung", ExpectedResult = true, Ignore = "Remove to run test case")>]
-[<TestCase("the quick brown fox", ExpectedResult = false, Ignore = "Remove to run test case")>]
-[<TestCase("Emily Jung Schwartzkopf", ExpectedResult = true, Ignore = "Remove to run test case")>]
-[<TestCase("éléphant", ExpectedResult = false, Ignore = "Remove to run test case")>]
+[<TestCase("eleven", ExpectedResult = false)>]
+[<TestCase("subdermatoglyphic", ExpectedResult = true)>]
+[<TestCase("Alphabet", ExpectedResult = false)>]
+[<TestCase("thumbscrew-japingly", ExpectedResult = true)>]
+[<TestCase("Hjelmqvist-Gryb-Zock-Pfund-Wax", ExpectedResult = true)>]
+[<TestCase("Heizölrückstoßabdämpfung", ExpectedResult = true)>]
+[<TestCase("the quick brown fox", ExpectedResult = false)>]
+[<TestCase("Emily Jung Schwartzkopf", ExpectedResult = true)>]
+[<TestCase("éléphant", ExpectedResult = false)>]
 let ``Isogram correctly detects isograms`` (actual: string) =
     isogram actual
