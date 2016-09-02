8521850f8e7efa43aa90d7a2b557c3a99d2df76c
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-09-02 12:43:08 +0200

    use native Char.IsDigit

--- PhoneNumber.fs
+++ PhoneNumber.fs
@@ -1,2 +1,24 @@
 ﻿module PhoneNumber
 
+open System
+
+let isDigit character =
+    ['0'..'9']
+    |> List.contains character
+
+let parse nb = 
+    nb
+    |> List.ofSeq
+    |> List.filter isDigit
+
+let toString = Array.ofList >> String
+
+let parsePhoneNumber number =
+    let parsed = parse number 
+    match (Seq.length parsed, parsed) with
+    | 10, relevant
+    | 11, '1' :: relevant ->
+        relevant
+        |> toString
+        |> Some
+    | _ -> None
