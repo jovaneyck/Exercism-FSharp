df1a61676ed54656d43e8f8c39e8b5d3062f6a43
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-09-14 15:36:08 +0200

    rewrote list ops with folds. Just for fun you know

--- list-ops/ListOps.fs
+++ list-ops/ListOps.fs
@@ -1,52 +1,37 @@
 ﻿module ListOps
 
-let length l =
-    let rec lngth acc =
-        function
-        | [] -> acc
-        | _ :: t -> lngth (acc + 1) t
-    lngth 0 l
-
-let reverse l = 
-    let rec rev acc = 
-        function
-        | [] -> acc
-        | x :: xs -> rev (x :: acc) xs
-    rev [] l
-
-let map f l =
-    let rec mapr acc =
-        function
-        | [] -> acc |> reverse
-        | x :: xs -> mapr (f x :: acc) xs
-    mapr [] l
-
-let filter f l = 
-    let rec filt acc =
-        function
-        | [] -> acc |> reverse
-        | x :: xs when f x -> filt (x :: acc) xs
-        | _ :: xs -> filt acc xs
-    filt [] l
-
 let rec foldl f seed = 
     function
     | [] -> seed
     | x :: xs -> foldl f (f seed x) xs
 
+let reverse l = 
+    let cons = (fun acc el -> el :: acc)
+    foldl cons [] l
+
 let rec foldr f seed l =
     foldl (fun a b -> f b a) seed (reverse l)
 
+let length l = 
+    foldr (fun _ s -> s + 1) 0 l
+
+let map f l =
+    let mapf el acc = (f el) :: acc
+    foldr mapf [] l
+
+let filter f l = 
+    let filt el acc = 
+        if 
+            f el 
+        then 
+            el :: acc 
+        else 
+            acc
+    foldr filt [] l
+
 let append xs ys = 
-    let rec apprev x y =
-        match x with
-        | [] -> y
-        | h :: t -> apprev t (h :: y)
-    apprev (reverse xs) ys
+    let app el acc = el :: acc
+    foldr app ys xs
 
 let concat l = 
-    let rec conc acc l = 
-        match l with
-        | [] -> reverse acc
-        | h :: t -> conc (append (reverse h) acc) t
-    conc [] l
+    foldr append [] l
