e8c46d03363de366fc98e1656fadbc2d48427fbb
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-09-26 16:57:24 +0200

    dotviz dsl

--- Scratchpad.fsx
+++ Scratchpad.fsx
@@ -1,1 +1,17 @@
-﻿
+﻿type Customer = 
+    {
+        Name : string
+        Age : int
+    }
+
+let bob = { Name = "Bob"; Age = 23 }
+
+type HttpResult<'a> =
+    | OK of Customer
+    | ServerError of 'a
+
+let result = ServerError 500
+
+match result with
+| OK {Name = n} -> ()
+| ServerError message -> ()
---
+++ dot-dsl/DotDsl.fs
@@ -0,0 +1,57 @@
+﻿module DotDsl
+
+type Attribute = { name: string; value: string }
+type Node = {name : string; attributes : Attribute list}
+type Edge = {start : string; stop : string; attributes : Attribute list}
+type Element =
+    | Node of Node
+    | Edge of Edge
+    | Attribute of Attribute
+    
+type Graph = Element list
+
+let graph elements : Graph = 
+    elements
+
+let buildAttribute (name,value) = 
+    {name = name; value = value}
+let attr name value = 
+    Attribute <| buildAttribute (name,value)
+
+let node name attributes = 
+    Node {name = name; attributes = (attributes |> List.map buildAttribute) }
+
+let edge start stop attributes = 
+    Edge {start = start; stop = stop; attributes = (attributes |> List.map buildAttribute)}
+
+let nodes g = 
+    let rec nodes' acc g = 
+        match g with
+        | [] -> 
+            acc
+            |> List.sortBy (fun n -> n.name)
+            |> List.map Node
+        | Node n :: t -> nodes' (n :: acc) t
+        | _ :: t -> nodes' acc t
+    nodes' [] g
+let edges g = 
+    let rec edges' acc g = 
+        match g with
+        | [] -> 
+            acc
+            |> List.sortBy (fun e -> e.start)
+            |> List.map Edge
+        | Edge e :: t -> edges' (e :: acc) t
+        | _ :: t -> edges' acc t
+    edges' [] g
+
+let attrs g = 
+    let rec attrs' acc g = 
+            match g with
+            | [] -> 
+                acc
+                |> List.sortBy (fun (a : Attribute) -> a.name)
+                |> List.map Attribute
+            | Attribute a :: t -> attrs' (a :: acc) t
+            | _ :: t -> attrs' acc t
+    attrs' [] g
