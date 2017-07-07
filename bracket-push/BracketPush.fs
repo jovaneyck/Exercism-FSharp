8c91d120f919bfa4c619cbcb84c135a93b962111
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-07-07 20:28:23 +0200

    bracket matching using active patterns and a map

--- bracket-push/BracketPush.fs
+++ bracket-push/BracketPush.fs
@@ -1,24 +1,39 @@
 ﻿module BracketPush
 
 let matched brackets = 
+    let pairs = 
+        [('[',']')
+         ('{','}')
+         ('(',')')]
+    let opens = pairs |> List.map fst
+    let closes = pairs |> List.map snd
+    let openBracketFor =
+        pairs
+        |> List.map (fun (o,c) -> (c,o))
+        |> Map.ofList
+
+    let (|Open|_|) character = 
+        if opens |> List.contains character then
+            Some (Open character)
+        else
+            None
+    let (|Close|_|) character = 
+        if closes |> List.contains character then
+            Some (Close character)
+        else
+            None
+
+    let openingBracketFor closingBracket = 
+        openBracketFor |> Map.find closingBracket
+
     let rec matchedRec stack list = 
         match list, stack with
-        | [], [] -> true
-        | '[' :: bs, _          -> matchedRec ('[' :: stack) bs
-        | ']' :: bs, '[' :: ss  -> matchedRec ss bs
-        | ']' :: _, _           -> false
-
-        | '{' :: bs, _          -> matchedRec ('{' :: stack) bs
-        | '}' :: bs, '{' :: ss  -> matchedRec ss bs
-        | '}' :: _, _           -> false
-
-        | '(' :: bs, _          -> matchedRec ('(' :: stack) bs
-        | ')' :: bs, '(' :: ss  -> matchedRec ss bs
-        | ')' :: _, _           -> false
-        
-        | _ :: bs, _            -> matchedRec stack bs
-
-        | _                     -> false
+        | [], []                                                -> true
+        | Open o :: bs, _                                       -> matchedRec (o :: stack) bs
+        | Close c :: bs, o :: ss when o = openingBracketFor c   -> matchedRec ss bs
+        | Close _ :: _, _                                       -> false
+        | _ :: bs, _                                            -> matchedRec stack bs
+        | _                                                     -> false
 
     let list = brackets |> Seq.toList
     matchedRec [] list
