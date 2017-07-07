c45d7cd64b213850ca9585fa6f2d142ad6abbe68
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-07-07 17:45:37 +0200

    matched bracket using extensive pattern matching

---
+++ bracket-push/BracketPush.fs
@@ -0,0 +1,24 @@
+﻿module BracketPush
+
+let matched brackets = 
+    let rec matchedRec stack list = 
+        match list, stack with
+        | [], [] -> true
+        | '[' :: bs, _          -> matchedRec ('[' :: stack) bs
+        | ']' :: bs, '[' :: ss  -> matchedRec ss bs
+        | ']' :: _, _           -> false
+
+        | '{' :: bs, _          -> matchedRec ('{' :: stack) bs
+        | '}' :: bs, '{' :: ss  -> matchedRec ss bs
+        | '}' :: _, _           -> false
+
+        | '(' :: bs, _          -> matchedRec ('(' :: stack) bs
+        | ')' :: bs, '(' :: ss  -> matchedRec ss bs
+        | ')' :: _, _           -> false
+        
+        | _ :: bs, _            -> matchedRec stack bs
+
+        | _                     -> false
+
+    let list = brackets |> Seq.toList
+    matchedRec [] list
