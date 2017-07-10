a9309b9207c7d61856c7255c0580cf140a69166e
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-07-10 21:15:54 +0200

    Coin change kata. Functionally correct but does not scale. AT. ALL.

---
+++ change/Change.fs
@@ -0,0 +1,26 @@
+﻿module Change
+
+let rec findChange amount denominations =
+    if amount < 0 then
+        None
+    elif amount = 0 then
+        Some <| [[]]
+    else
+        match denominations with
+        | [] -> None
+        | d :: ds when d <= amount -> 
+            let ignored = (findChange amount ds) 
+            let used = (findChange (amount - d) (d::ds))
+
+            match ignored, used with
+            | None, None -> None
+            | None, Some b -> Some (b |> List.map(fun r -> d :: r))
+            | s, None -> s
+            | Some a, Some b -> Some (a @ (b |> List.map(fun r -> d :: r)))
+        | _ :: ds -> findChange amount ds    
+
+let change amount denominations = 
+    let sortedDenominations = denominations |> List.sortDescending
+    let coins = findChange amount sortedDenominations
+    coins  
+    |> Option.map (List.minBy List.length >> List.sort)
