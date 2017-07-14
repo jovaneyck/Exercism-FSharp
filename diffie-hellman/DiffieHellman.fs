34aacc63eeb796a8134daca7121e648a40468c27
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-07-14 17:27:59 +0200

    diffie-hellman

---
+++ diffie-hellman/DiffieHellman.fs
@@ -0,0 +1,14 @@
+﻿module DiffieHellman
+
+open System
+open System.Numerics
+
+let modpow a n modulus = BigInteger.ModPow(a,n,modulus)
+
+let rng = new Random()
+let privateKey (prime : BigInteger) = 
+    rng.Next(1, int prime - 1) //TODO: not really using BigInt primes ey?
+    |> BigInteger
+
+let publicKey p g pk = modpow g pk p
+let secret p pub priv = modpow pub priv p
