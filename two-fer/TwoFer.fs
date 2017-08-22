37bb8249ed75b244e22bb59fb9dc654a9564fa49
Jo Van Eyck <ext_jvy@belux.conseur.org>
2017-08-22 16:13:19 +0200

    Two-fer

---
+++ two-fer/TwoFer.fs
@@ -0,0 +1,9 @@
+﻿module TwoFer
+
+let flip f x y = f y x
+let orDefault = flip defaultArg
+
+let getResponse name = 
+    name
+    |> orDefault "you"
+    |> (sprintf "One for %s, one for me.")
---
+++ two-fer/TwoFerTest.fs
@@ -0,0 +1,17 @@
+﻿module TwoFerTest
+
+open NUnit.Framework
+
+open TwoFer
+
+[<Test>]
+let ``No name given`` () =
+    Assert.That(getResponse None, Is.EqualTo("One for you, one for me."))
+
+[<Test>]
+let ``A name given`` () =
+    Assert.That(getResponse (Some "Alice"), Is.EqualTo("One for Alice, one for me."))
+
+[<Test>]
+let ``Another name given`` () =
+    Assert.That(getResponse (Some "Bob"), Is.EqualTo("One for Bob, one for me."))
