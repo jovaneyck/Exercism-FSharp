79d0496c40c44d3eae4001162c09c1e7f0077fe3
Jo Van Eyck <jo.vaneyck@ae.be>
2018-04-06 17:39:28 +0200

    collatz with tailrec function. Tried an unfold first but that ended up being way too complicated

---
+++ collatz-conjecture/CollatzConjecture.fs
@@ -0,0 +1,17 @@
+﻿module CollatzConjecture
+
+let next number = 
+    if number % 2 = 0 then
+        number / 2
+    else 
+        1 + 3 * number
+
+let steps (number: int): int option = 
+    let rec stepsAcc acc n =
+        match n with
+        | n when n <= 0 -> None
+        | n when n = 1 -> Some acc
+        | n -> 
+            let nextValue = next n
+            stepsAcc (acc + 1) nextValue
+    stepsAcc 0 number
